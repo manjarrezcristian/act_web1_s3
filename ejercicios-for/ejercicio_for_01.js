03a95f5d3b57ead5df65cf20ced7aafb9844bcca
JeritoX10 <jpareja@cesde.net>
2025-07-24 20:52:05 -0500

    primer_ejercicio

--- ejercicios-for/ejercicio_for_01.js
+++
@@ -1,3 +0,0 @@
-// Ejercicio For 1: Tabla de Multiplicar Completa
-// Usa ciclos for anidados para crear las tablas de multiplicar del 1 al 12. 
-// Presenta los resultados en formato de tabla organizada.
---
+++ ejercicios-for/src/ejercicio_for_01.js
@@ -0,0 +1,14 @@
+//Crea un programa que use un ciclo while para mostrar una
+//  cuenta regresiva desde 20 hasta 1, mostrando 
+// cada número en la consola. Al final debe mostrar "¡Despegue!".
+
+
+let contador = 20;
+
+while (contador >= 1) {
+  console.log(contador);
+  contador--; 
+}
+
+
+console.log("¡Despegue!");
